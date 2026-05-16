7d15b14297feaa6df8f254b9683a5b965f15549f
hant12 <hant12@vng.com.vn>
2026-05-16 17:42:37 +0700

    new update

---
+++ src/settings.1c892.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/loadingScene.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",resources:"e1959",main:"189d5"}};
